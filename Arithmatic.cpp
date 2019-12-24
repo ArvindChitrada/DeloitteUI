e72906533fa7c67b53b25cd50515dd72495604b6
ArvindChitrada <59163312+ArvindChitrada@users.noreply.github.com>
2019-12-24 02:48:42 -0800

    Update Arithmatic.cpp

--- Arithmatic.cpp
+++ Arithmatic.cpp
@@ -1,111 +1,65 @@
-//person-->employee-->manager
 #include<iostream>
 using namespace std;
 
-class Person{
-	char name[30],city[30],gender[10];
-	int exp;
+class Arithmetic{
+	int number1;
+	int number2;
+	int result;
 	public:
-		void getDetails(){
-			cin>>name;
-			cin>>city;
-			cin>>gender;
-			cin>>exp;
-			cout<<"\n";
+		void getNumbers(){
+			cout<<"Enter any two numbers \n";
+			cin>>number1;
+			cin>>number2;
 		}
-		void printDetails(){
-			cout<<"Name is : "<<name<<endl;
-			cout<<"city is : "<<city<<endl;
-			cout<<"Gender is : "<<gender<<endl;
+		void addition(){
+			getNumbers();
+			result=number1+number2;
+			cout<<"Addition result "<<result;
 		}
-		char* Name(){
-			return name;
+		void subtraction(){
+			getNumbers();
+			result=number1-number2;
+			cout<<"Subtraction result "<<result;
 		}
-		int exper(){
-			return exp;
+		void multiplication(){
+			getNumbers();
+			result=number1*number2;
+			cout<<"Multiplication result "<<result;
 		}
-		
-};
-
-class Employee:Person{
-	char designation[30];
-	public:
-		void getDesignation(){
-			getDetails();
-			cout<<"Enter "<<Person::Name()<<"'s designation"<<endl;
-			cin>>designation;
-			
-		}
-		void printDesignation(){
-			printDetails();
-			
-			cout<<"Designation of "<<Person::Name()<<" is : "<<designation<<endl;
-		}
-		char* HisName(){
-			return Person::Name();
-		}
-		int experience(){
-			return Person::exper();
+		void division(){
+			getNumbers();
+			result=number1/number2;
+			cout<<"Division result "<<result;
 		}
 };
-
-class Manager:Employee{
-	char manager[30];
-	public:
-		void getManagerName(){
-			
-			getDesignation();
-			cout<<"Enter "<<Employee::HisName()<<" manager's name"<<endl;
-			cin>>manager;
-			cout<<"\n";
+int main(){
+	int input;
+	Arithmetic arithmetic;
+	cout<<"Hello, Choose an option between 1-4 to perform an arithmetic operation \n"<<endl;
+	cout<<" 1 for Addition \n 2 for Subtraction \n 3 for Multiplication \n 4 for Division \n";
+	cout<<"Please enter your input here: ";
+	cin>>input;
+	switch (input){
+		case 1:{
+			arithmetic.addition();
+			break;
 		}
-		void printManagerName(){
-			
-			printDesignation();
-			
-			cout<<"Manager of "<<Employee::HisName()<<" employee is : "<<manager<<endl;
+		case 2:{
+			arithmetic.subtraction();
+			break;
 		}
-		char getManagerEligibility(){
-			char option;
-			cout<<"Do you want to check your eligibility for a manager post press Y or N"<<endl;
-			cin>>option;
-			return option;
+		case 3:{
+			arithmetic.multiplication();
+			break;
 		}
-		void printManagerEligibility(){
-			if(Employee::experience()>10){
-				cout<<"Congarts! "<<Employee::HisName()<<" you can be a manager. \n"<<endl;
-			}
-			else{
-				cout<<"Sorry "<<Employee::HisName()<<" Insufficient experience. \n"<<endl;
-			}
+		case 4:{
+			arithmetic.division();
+			break;
 		}
-};
-
-main(){
-	char option;
-	Manager m[3];
-	for(int i=0;i<2;i++){
-		
-		cout<<"Enter details of the person like name city and gender and exp"<<endl;
-		m[i].getManagerName();
-		option=m[i].getManagerEligibility();
-		switch(option){
-			case 'Y':{
-				m[i].printManagerEligibility();
-				break;
-			}
-			case 'N':{
-				m[i].printManagerEligibility();
-				break;
-			}
-			default:{
-				cout<<"Please choose only between Y or N"<<endl;
-				break;
-			}
-		m[i].printManagerName();
-		
-		cout<<"\n";
-	}
+		default:{
+			cout<<"Wrong choice... Please select between 1-4";
+			break;
+		}
 	}
 	
 }
