5787aa339b430e7b8fca0036afd273678a26c565
ArvindChitrada <59163312+ArvindChitrada@users.noreply.github.com>
2019-12-24 02:49:08 -0800

    Update Multiinherit.cpp

--- Multiinherit.cpp
+++ Multiinherit.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 class Person{
 	char name[30],city[30],gender[10];
+	int exp;
 	public:
 		void getDetails(){
 			cin>>name;
 			cin>>city;
 			cin>>gender;
+			cin>>exp;
+			cout<<"\n";
 		}
 		void printDetails(){
 			cout<<"Name is : "<<name<<endl;
@@ -17,6 +20,9 @@ class Person{
 		}
 		char* Name(){
 			return name;
+		}
+		int exper(){
+			return exp;
 		}
 		
 };
@@ -38,6 +44,9 @@ class Employee:Person{
 		char* HisName(){
 			return Person::Name();
 		}
+		int experience(){
+			return Person::exper();
+		}
 };
 
 class Manager:Employee{
@@ -48,6 +57,7 @@ class Manager:Employee{
 			getDesignation();
 			cout<<"Enter "<<Employee::HisName()<<" manager's name"<<endl;
 			cin>>manager;
+			cout<<"\n";
 		}
 		void printManagerName(){
 			
@@ -55,13 +65,47 @@ class Manager:Employee{
 			
 			cout<<"Manager of "<<Employee::HisName()<<" employee is : "<<manager<<endl;
 		}
+		char getManagerEligibility(){
+			char option;
+			cout<<"Do you want to check your eligibility for a manager post press Y or N"<<endl;
+			cin>>option;
+			return option;
+		}
+		void printManagerEligibility(){
+			if(Employee::experience()>10){
+				cout<<"Congarts! "<<Employee::HisName()<<" you can be a manager. \n"<<endl;
+			}
+			else{
+				cout<<"Sorry "<<Employee::HisName()<<" Insufficient experience. \n"<<endl;
+			}
+		}
 };
 
 main(){
+	char option;
 	Manager m[3];
 	for(int i=0;i<2;i++){
-		cout<<"Enter details of the person like name city and gender "<<endl;
+		
+		cout<<"Enter details of the person like name city and gender and exp"<<endl;
 		m[i].getManagerName();
+		option=m[i].getManagerEligibility();
+		switch(option){
+			case 'Y':{
+				m[i].printManagerEligibility();
+				break;
+			}
+			case 'N':{
+				m[i].printManagerEligibility();
+				break;
+			}
+			default:{
+				cout<<"Please choose only between Y or N"<<endl;
+				break;
+			}
 		m[i].printManagerName();
+		
+		cout<<"\n";
 	}
+	}
+	
 }
